628c3670c9f8af65ddb0b2eeb2d53815781dacda
fannyhasbi <fnyhsbi@gmail.com>
2018-09-11 00:14:59 +0700

    Add Flight package

--- index.php
+++ index.php
@@ -4,3 +4,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+Flight::route('GET /', function(){
+  Flight::render('home');
+});
+
+Flight::start();
---
+++ views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Evma API</title>
+</head>
+<body>
+  <h1>Evma API</h1>
+  <h2>Event Management Package for handling API</h2>
+</body>
+</html>
